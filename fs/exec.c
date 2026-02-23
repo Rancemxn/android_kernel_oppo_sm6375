34e8e3afc96fcbfe268992a541383ae5b40d9f60
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2026-02-23 19:01:38 +0800

    fix do_execve
    Signed-off-by: Rancemxn <209220634+Rancemxn@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1934,9 +1934,7 @@ int do_execve_file(struct file *file, void *__argv, void *__envp)
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
-#endif
+
 
 	return __do_execve_file(AT_FDCWD, NULL, argv, envp, 0, file);
 }
@@ -1947,6 +1945,9 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1974,11 +1975,9 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
-
 #ifdef CONFIG_KSU_MANUAL_HOOK // 32-bit ksud and 32-on-64 support
 	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
 #endif
-
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
